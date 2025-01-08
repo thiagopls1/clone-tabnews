ba14c75d9f231bf3833691f060690ff2c8e8dada
thiagopls1 <thiagopls1@hotmail.com>
2025-01-08 14:34:53 -0300

    Add `database.js`

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Client } from "pg";
+
+async function query(queryObj) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+  await client.connect();
+  const result = await client.query(queryObj);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
-// api/v1/status
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ status: "tá funcionando" });
 }
 
