c3606f199ec54acb51a6e8d8b2de18712c88f4bb
thiagopls1 <thiagopls1@hotmail.com>
2025-01-27 17:27:47 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,4 +1,4 @@
-import fsPromises from "node:fs/promises";
+import fs from "node:fs";
 import { join } from "node:path";
 import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
@@ -8,30 +8,43 @@ beforeAll(async () => {
   await cleanDatabase();
 });
 
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous User", () => {
+    describe("Running pending migrations", () => {
+      const migrationsFiles = fs.readdirSync(join("infra", "migrations"));
+      const migrationsCount = migrationsFiles.length;
+
+      test("For the first time", async () => {
+        // First Request
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
+        const response1Body = await response.json();
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBe(migrationsCount);
+      });
+
+      test("For the second time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(response.status).toBe(200);
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(migrationsCount - 1);
+      });
+    });
+  });
+});
+
 async function cleanDatabase() {
   await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
 }
-
-test("POST to /api/v1/migrations should return 200", async () => {
-  // First Request
-  const migrationsFiles = await fsPromises.readdir(join("infra", "migrations"));
-  const migrationsCount = migrationsFiles.length;
-
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response1.status).toBe(201);
-  const response1Body = await response1.json();
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBe(migrationsCount);
-
-  // Second Request
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-
-  expect(response2.status).toBe(200);
-  const response2Body = await response2.json();
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(migrationsCount - 1);
-});
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,17 +4,21 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous User", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
 
-  // Expect date to be valid
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+      expect(response.status).toBe(200);
+      // Expect date to be valid
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toBe(parsedUpdatedAt);
 
-  // Expect default values to version and max connections database
-  expect(responseBody.dependencies.database.max_connections).toBe(100);
-  expect(responseBody.dependencies.database.version).toBe("16.0");
-  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+      // Expect default values to version and max connections database
+      expect(responseBody.dependencies.database.max_connections).toBe(100);
+      expect(responseBody.dependencies.database.version).toBe("16.0");
+      expect(responseBody.dependencies.database.opened_connections).toBe(1);
+    });
+  });
 });
