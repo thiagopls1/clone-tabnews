de4116bee86e84562c4466b8f59cfcf4bed465b3
thiagopls1 <thiagopls1@hotmail.com>
2026-07-07 13:46:52 -0300

    feat: create hash `password` in `user.create`

---
+++ models/password.js
@@ -0,0 +1,26 @@
+import bcryptjs from "bcryptjs";
+
+async function hash(password) {
+  const rounds = getNumberOfRounds();
+  return await bcryptjs.hash(password, rounds);
+}
+
+function getNumberOfRounds() {
+  const rounds = 1;
+  if (process.env.NODE_ENV === "production") {
+    rounds = 14;
+  }
+
+  return rounds;
+}
+
+async function compare(providedPassword, storedPassword) {
+  return await bcryptjs.compare(providedPassword, storedPassword);
+}
+
+const password = {
+  hash,
+  compare,
+};
+
+export default password;
